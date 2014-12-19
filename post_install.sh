a2577760883e10e13dbdbe3e3bfdfc06392d9c18
Gustavo F Carvalho <gugafer51@gmail.com>
2014-12-19 14:47:21 -0200

    Inclusão da função download_home_files em post_install.

--- post_install.sh
+++ post_install.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+function download_home_files(){
+  mkdir ~/github
+  git clone https://github.com/gustavofc/home.git ~/github/home
+  ln -s github/home/.bashrc 
+  ln -s github/home/.bash_aliases 
+  ln -s github/home/.vimrc
+  ln -s github/home/.vim
+  ln -s github/home/.toprc
+}
+
 function install_powerlines_fonts(){
   git clone https://github.com/powerline/fonts.git
   ./fonts/install.sh
 }
 
+
+
 # Main
+download_home_files
 install_powerlines_fonts
